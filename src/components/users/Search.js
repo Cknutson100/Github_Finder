ae7a38a49773bde38e58d188f68c567ecab71e1e
Christian Knutson <cknutson575@gmail.com>
2021-06-07 22:35:29 -0700

    Changes

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -11,12 +11,17 @@ export class Search extends Component {
         searchUsers: PropTypes.func.isRequired,
         clearUsers: PropTypes.func.isRequired,
         showClear: PropTypes.bool.isRequired,
+        setAlert: PropTypes.func.isRequired,
     }
 
     onSubmit = (e) => {
         e.preventDefault();
-        this.props.searchUsers(this.state.text);
-        this.setState({ text: ''});
+        if(this.state.text === '') {
+            this.props.setAlert('Please enter something', 'light')
+        } else {
+            this.props.searchUsers(this.state.text);
+            this.setState({ text: ''});
+        }
     }
 
     onChange = (e) => {
@@ -24,6 +29,7 @@ export class Search extends Component {
         // in this example, e.target.value will change the value of text='' in the state object above.
         this.setState({[e.target.name]: e.target.value});
     }
+
 
     render() {
         const { showClear, clearUsers } = this.props;
