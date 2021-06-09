a2abcff34e3580ec08377c896b32bd87756ceedd
Christian Knutson <cknutson575@gmail.com>
2021-06-08 21:41:18 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
-import React from 'react'
-import Navbar from './components/layout/Navbar.js'
-import Users from './components/users/Users.js'
-import Search from './components/users/Search.js'
-import axios from 'axios'
+import React from 'react';
+import Navbar from './components/layout/Navbar.js';
+import Users from './components/users/Users.js';
+import Search from './components/users/Search.js';
+import Alert from "/.components/layout/Alert.js";
+import axios from 'axios';
 import './App.css';
 
-class App extends React.Component{
+ class App extends React.Component{
   state = {
     users: [],
     loading: false,  // There is going to be a moment in time before we get data back. While its fetching Loading will be True.
     alert: null,     // This creates a piece of state that is default set to null for alert.
   }
-
-  // This lifecycle method, will fire off when the app is first run, when the App component "mounts"
-  // This is helpful if you ever want to make an API request right when the page loads this is the place to do it.
-  // Also notice this is how your perform an async/await api call with axios.
-  // async componentDidMount() {
-  //   this.setState({loading: true })
-
-  //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
-  //   client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-  //   console.log(res.data)
-
-  //   this.setState({users: res.data, loading:false}) // this is how you change a state's value.
-  // }
 
   // Search Github users:
   searchUsers = async (text) => {
@@ -36,21 +24,21 @@ class App extends React.Component{
 
     this.setState({users: res.data.items, loading:false}) // this is how you change a state's value.
   }
-
   // Clear users from state:
   clearUsers = () => this.setState({ users: [], loading: false, })
   // Set Alert taking in msg and type
   setAlert = (msg, type) => { 
-    this.setState({alert: {msg:msg, type:type}}); // When this function is called it will set alert state to the passed in parameters
+    this.setState({alert: {msg:msg, type:type}}); // When this function is called it will set alert state to the passed in parameters.
   }
 
     render() {
-      const { users, loading } = this.state;
+      const { users, loading, alert } = this.state;
 
       return (
         <div className="App">
           <Navbar />
           <div className="container">
+            <Alert alert={alert}/>
             <Search searchUsers={this.searchUsers} 
             clearUsers={this.clearUsers} 
             showClear={users.length > 0 ? true : false}
@@ -66,3 +54,17 @@ class App extends React.Component{
 
 
 export default App;
+
+
+  // This lifecycle method, will fire off when the app is first run, when the App component "mounts"
+  // This is helpful if you ever want to make an API request right when the page loads this is the place to do it.
+  // Also notice this is how your perform an async/await api call with axios.
+  // async componentDidMount() {
+  //   this.setState({loading: true })
+
+  //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
+  //   client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+  //   console.log(res.data)
+
+  //   this.setState({users: res.data, loading:false}) // this is how you change a state's value.
+  // }
---
+++ src/components/layout/Alert.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Alert = ({ alert }) => {
+    return (
+        // This part of the code makes it so that if alert is activated then it shows the div
+        alert !== null && (
+            // This part of the code makes sure that the className is dynamic for the type of alert we want to use from CSS
+            // Additionally the icon is from font.awesome and the object pulls the message from the alert prop, we pulled in.
+            <div className={`alert alert-${alert.type}`}>
+                <i className="fas fa-info-circle"></i> {alert.msg}
+            </div>
+        )
+    )
+}
+
+export default Alert
